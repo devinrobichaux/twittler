7c242d8c7e290ea90ea1a692ca430b1cdbdaf834
Melinda Sarnicki <melindasarnicki@gmail.com>
2015-06-08 12:50:20 -0400

    implement updateTime function to add timestamp to each tweet

---
+++ timeSince.js
@@ -0,0 +1,34 @@
+/* code snippet courtsey of Sky Sanders 
+   reference: http://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site */ 
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes ago";
+    }
+    interval = Math.floor(seconds);
+    if (interval > 1) {
+        return interval + " seconds ago";
+    }
+    interval = Math.floor(seconds / 0);
+    return "now";
+}
